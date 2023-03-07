72d8d9c0986a0fab27cc0cd73cc50df3555af291
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-03-07 16:09:51 +0100

    added migrations table

--- database/migrations/2023_03_07_144613_create_projects_table.php
+++ database/migrations/2023_03_07_144613_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100)->unique();
+            $table->text('content')->nullable();
+            $table->string('slug');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
